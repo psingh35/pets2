7b37e99835fdbc25ea2f507e5dbcbe93e37627d0
Parminder Singh <psingh35@mail.greenriver.edu>
2018-01-18 17:42:09 -0500

    add additional routes for fourms

--- index.php
+++ index.php
@@ -16,14 +16,38 @@ $f3->route('GET /', function()
     ('views/home.html');
 }
 );
+$f3->route('GET /order',
+function()
+{
+  echo '<h1> Form1</h1>';
+}
+);
 
-//Run fat free
-$f3->run();
+$f3->route('GET /order2',
+    function()
+    {
+        echo '<h1> form2</h1>';
+    }
+);
+
+$f3->route('GET /results',
+    function()
+    {
+        echo '<h1> Results</h1>';
+    }
+);
+
+//create an order route
+$f3->route('GET /order', function ()
+{
+    echo '<h1></h1> Add  the show order</h1>';
+});
+
 
 //Define order page
 $f3->route('GET /pets/show', function($f3, $params)
 {
-
+ //$f3->set('cat', @$params['cat']);
 }
 );
 
@@ -32,4 +56,15 @@ $f3->route('GET /pets/show/cat', function($f3, $params)
 {
     echo '<img src="http://i.imgur.com/cpsQq.jpg" alt="cat">';
 }
+
 );
+//Define a default rout
+$f3->route('GET /pets/animal/show', function ()
+{
+    $template = new Template();
+    echo $template ->render ('views/Jewelry.html');
+}
+);
+
+//Run fat free
+$f3->run();
