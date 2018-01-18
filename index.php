339707766fa689704d1d1b1335a648a9bac2a345
jbosegre <jbose@mail.greenriver.edu>
2018-01-18 17:11:19 -0500

    Created the composer.json file, index.php file, and the home.html with view directory

--- index.php
+++ index.php
@@ -1,2 +1,35 @@
 <?php
-echo "welcome to the project";
+
+ini_set ("display_errors", 1);
+error_reporting(E_ALL);
+
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//define a default route
+$f3->route('GET /', function()
+{
+    $view =  new View;
+    echo $view->render
+    ('views/home.html');
+}
+);
+
+//Run fat free
+$f3->run();
+
+//Define order page
+$f3->route('GET /pets/show', function($f3, $params)
+{
+
+}
+);
+
+//Define order page for cats
+$f3->route('GET /pets/show/cat', function($f3, $params)
+{
+    echo '<img src="http://i.imgur.com/cpsQq.jpg" alt="cat">';
+}
+);
