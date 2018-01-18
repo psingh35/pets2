274ee751a6065bc8800f0f17a612f88d0e977b77
jbosegre <jbose@mail.greenriver.edu>
2018-01-18 18:34:45 -0500

    made some fixes on the routing

--- index.php
+++ index.php
@@ -8,62 +8,31 @@ require_once('vendor/autoload.php');
 //Create an instance of the Base class
 $f3 = Base::instance();
 
+//global view directory
+$template =  new Template();
+
 //define a default route
 $f3->route('GET /', function()
 {
-    $view =  new View;
-    echo $view->render
-    ('views/home.html');
+    global $template;
+    echo $template->render('views/form1.html');
 }
 );
+
 $f3->route('GET /order',
 function()
 {
-  echo '<h1> Form1</h1>';
+    global $template;
+    echo $template->render('views/form2.html');
 }
 );
 
-$f3->route('GET /order2',
+$f3->route('POST /order2',
     function()
     {
-        echo '<h1> form2</h1>';
+        global $template;
+        echo $template->render('views/results.html');
     }
-);
-
-$f3->route('GET /results',
-    function()
-    {
-        echo '<h1> Results</h1>';
-    }
-);
-
-//create an order route
-$f3->route('GET /order', function ()
-{
-    echo '<h1></h1> Add  the show order</h1>';
-});
-
-
-//Define order page
-$f3->route('GET /pets/show', function($f3, $params)
-{
- //$f3->set('cat', @$params['cat']);
-}
-);
-
-//Define order page for cats
-$f3->route('GET /pets/show/cat', function($f3, $params)
-{
-    echo '<img src="http://i.imgur.com/cpsQq.jpg" alt="cat">';
-}
-
-);
-//Define a default rout
-$f3->route('GET /pets/animal/show', function ()
-{
-    $template = new Template();
-    echo $template ->render ('views/Jewelry.html');
-}
 );
 
 //Run fat free
